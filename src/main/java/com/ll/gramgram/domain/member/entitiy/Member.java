5aab89c1657b925a2c9dcc890dc4724dd4a6a624
bw1611 <bw1611@naver.com>
2023-03-30 14:28:17 +0900

    getGrantedAuthorities, CustomUserDetailsService 추가

--- src/main/java/com/ll/gramgram/domain/member/entitiy/Member.java
+++ src/main/java/com/ll/gramgram/domain/member/entitiy/Member.java
@@ -4,14 +4,19 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 public class Member {
 
@@ -19,6 +24,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
     private String password;
 
@@ -27,4 +33,16 @@ public class Member {
 
     @CreatedDate
     private LocalDateTime modifyDate;
+
+    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        if ("admin".equals(username)) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
+
+        return grantedAuthorities;
+    }
 }
