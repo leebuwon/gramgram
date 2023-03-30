3374b986c5fff22e64a7d22c61040979be5ad908
bw1611 <bw1611@naver.com>
2023-03-30 11:23:23 +0900

    회원가입 test 케이스 추가

--- src/main/java/com/ll/gramgram/domain/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram/domain/member/controller/MemberController.java
@@ -1,8 +1,16 @@
 package com.ll.gramgram.domain.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,8 +24,20 @@ public class MemberController {
         return "usr/member/join";
     }
 
+    @Getter
+    @AllArgsConstructor
+    private static class JoinForm{
+        @NotBlank
+        @Size(min = 4, max =  30)
+        private final String username;
+
+        @NotBlank
+        @Size(min = 4, max =  30)
+        private final String password;
+    }
+
     @PostMapping("/join")
-    public String join() {
+    public String join(@Valid JoinForm joinForm) {
         return "redirect:/";
     }
 }
