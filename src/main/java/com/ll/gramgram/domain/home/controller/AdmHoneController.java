9b15cd11e019fe7e2d4633c6565edcb6b784e36c
bw1611 <bw1611@naver.com>
2023-03-31 09:57:53 +0900

    Test session 추가, admin 페이지 추가

---
+++ src/main/java/com/ll/gramgram/domain/home/controller/AdmHoneController.java
@@ -0,0 +1,19 @@
+package com.ll.gramgram.domain.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/adm")
+public class AdmHoneController {
+
+    @GetMapping("")
+    @PreAuthorize("hasAnyAuthority('admin')")
+    public String showMain(){
+        return "adm/home/main";
+    }
+}
