e1e240bed402bf178dc3306f425a47d61f438bed
bw1611 <bw1611@naver.com>
2023-04-02 13:34:46 +0900

    insta 입력 폼 시작

--- src/test/java/com/ll/gramgram/domain/member/controller/InstaMemberControllerTest.java
+++ src/test/java/com/ll/gramgram/domain/member/controller/InstaMemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.domain.member.controller;
 
+import com.ll.gramgram.domain.member.entitiy.Member;
 import com.ll.gramgram.domain.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -59,7 +63,7 @@ class InstaMemberControllerTest {
 
     @Test
     @DisplayName("로그인을 안하고 인스타회원 정보 입력 페이지에 접근하면 로그인 페이지로 302")
-    void t002() throws Exception {
+    void t003() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
                 .perform(get("/instaMember/connect"))
@@ -71,5 +75,27 @@ class InstaMemberControllerTest {
                 .andExpect(handler().methodName("showConnect"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/member/login**"));
+    }
+
+    @Test
+//    @Rollback(value = false)
+    @DisplayName("인스타 회원 정보 입력 폼 처리")
+    @WithUserDetails("user1")
+    void t002() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/instaMember/connect")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "abc123")
+                        .param("gender", "W")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(InstaMemberController.class))
+                .andExpect(handler().methodName("connect"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/pop**"));
     }
 }
